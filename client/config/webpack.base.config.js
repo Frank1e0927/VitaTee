38c7278ac0c6800d4d38b09c4f322662378ab39f
frankie-mbp <877672473@qq.com>
2018-07-31 23:51:00 +0800

    loader

---
+++ client/config/webpack.base.config.js
@@ -0,0 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+
+module.exports = {
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: MiniCssExtractPlugin.loader
+          },
+          "css-loader"
+        ]
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({ 
+      template: './src/index.html', 
+      filename: './index.html' 
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css",
+      chunkFilename: "[id].css"
+    })
+  ]
+}
