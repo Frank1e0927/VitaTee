cc28297650c6ca4ff02b76a3d8417a616db52f3f
frankie-mbp <877672473@qq.com>
2018-08-07 20:56:04 +0800

    fix babel

--- client/src/container/Header/index.jsx
+++ client/src/container/Header/index.jsx
@@ -1,11 +1,27 @@
 import React from 'react'
 import './index.scss'
 
-const Header = () => (
-  <div className="header">
-    <div className="header-controller"></div>
-    <div className="header-half-left"></div>
-    <div className="header-half-right"></div>
-  </div>
-)
+class Header extends React.Component {
+  handleClick = () => {
+    console.log('123')
+  }
+  render() {
+    return (
+      <div className="header">
+        <Controller handleClick={this.handleClick}/>
+        <div className="header-half-left" />
+        <div className="header-half-right" />
+      </div>
+    )
+  }
+}
 export default Header
+
+class Controller extends React.Component {
+  render() {
+    const { handleClick } = this.props
+    return(
+      <div className="header-controller" onClick={handleClick} />
+    )
+  }
+}
