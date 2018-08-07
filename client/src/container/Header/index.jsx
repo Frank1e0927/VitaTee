538a734f01f616633140c26f581ca1d5ce01bd62
frankie-mbp <877672473@qq.com>
2018-08-07 21:33:58 +0800

    add dependenices

--- client/src/container/Header/index.jsx
+++ client/src/container/Header/index.jsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import { tween } from 'popmotion';
+import { MotionValue } from 'popmotion-react';
 import './index.scss'
 
 class Header extends React.Component {
@@ -8,7 +10,37 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <Controller handleClick={this.handleClick}/>
+        <MotionValue
+          onStateChange={{
+            on: ({ value }) => tween({
+              from: value.get(),
+              to: 100
+            }).start(value),
+            off: ({ value }) => tween({
+              from: value.get(),
+              to: 0
+            }).start(value)
+          }}
+        >
+          {({ v, state, setStateTo }) => (
+            <div
+              style={{
+                background: 'red',
+                width: '100px',
+                height: '100px',
+                transform: 'translateX(' + v + 'px)'
+              }}
+              onClick={(state === 'on') ? setStateTo.off : setStateTo.on}
+            />
+          )}
+        </MotionValue>
+        <Controller 
+          handleClick={this.handleClick}
+          // style={{
+          //   height: 
+          // }}
+          wraperRef={ref => this.controllerRef = ref}
+        />
         <div className="header-half-left" />
         <div className="header-half-right" />
       </div>
@@ -19,9 +51,13 @@ export default Header
 
 class Controller extends React.Component {
   render() {
-    const { handleClick } = this.props
+    const { handleClick, wraperRef } = this.props
     return(
-      <div className="header-controller" onClick={handleClick} />
+      <div
+        className="header-controller"
+        onClick={handleClick}
+        ref={ref => wraperRef(ref)}
+      />
     )
   }
 }
