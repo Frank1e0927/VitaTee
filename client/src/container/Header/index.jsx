f2854d6e6e61e959b9c9590c9577100847715d66
frankie-mbp <877672473@qq.com>
2018-08-07 20:14:19 +0800

    header controller

--- client/src/container/Header/index.jsx
+++ client/src/container/Header/index.jsx
@@ -2,6 +2,10 @@ import React from 'react'
 import './index.scss'
 
 const Header = () => (
-  <div className="header">i am header now</div>
+  <div className="header">
+    <div className="header-controller"></div>
+    <div className="header-half-left"></div>
+    <div className="header-half-right"></div>
+  </div>
 )
 export default Header
