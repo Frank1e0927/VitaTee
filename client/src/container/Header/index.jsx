e57ec837f806b5c8d9b32333dc79e4fa5b0f8a9d
frankie-mbp <877672473@qq.com>
2018-08-08 09:41:25 +0800

    fix

--- client/src/container/Header/index.jsx
+++ client/src/container/Header/index.jsx
@@ -1,6 +1,4 @@
 import React from 'react'
-import { tween } from 'popmotion';
-import { MotionValue } from 'popmotion-react';
 import './index.scss'
 
 class Header extends React.Component {
@@ -10,35 +8,8 @@ class Header extends React.Component {
   render() {
     return (
       <div className="header">
-        <MotionValue
-          onStateChange={{
-            on: ({ value }) => tween({
-              from: value.get(),
-              to: 100
-            }).start(value),
-            off: ({ value }) => tween({
-              from: value.get(),
-              to: 0
-            }).start(value)
-          }}
-        >
-          {({ v, state, setStateTo }) => (
-            <div
-              style={{
-                background: 'red',
-                width: '100px',
-                height: '100px',
-                transform: 'translateX(' + v + 'px)'
-              }}
-              onClick={(state === 'on') ? setStateTo.off : setStateTo.on}
-            />
-          )}
-        </MotionValue>
         <Controller 
           handleClick={this.handleClick}
-          // style={{
-          //   height: 
-          // }}
           wraperRef={ref => this.controllerRef = ref}
         />
         <div className="header-half-left" />
